63f7e83b2b4e54c69a3ab12aea4bea5d9e84528a
Alberto Brigandì <a.brigandi@reply.it>
2016-02-01 15:36:04 +0100

    Renamed two classes to comply with google code style

--- workflow-manager-spring/src/main/java/it/reply/workflowManager/spring/orchestrator/jms/AuditJmsProcessorBean.java
+++ workflow-manager-spring/src/main/java/it/reply/workflowManager/spring/orchestrator/jms/AuditJmsProcessorBean.java
@@ -1,12 +1,12 @@
 package it.reply.workflowManager.spring.orchestrator.jms;
 
-import javax.annotation.PostConstruct;
-import javax.persistence.EntityManagerFactory;
+import it.reply.workflowManager.spring.orchestrator.annotations.WorkflowPersistenceUnit;
 import org.jbpm.process.audit.jms.AsyncAuditLogReceiver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import it.reply.workflowManager.spring.orchestrator.annotations.WorkflowPersistenceUnit;
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManagerFactory;
 
 /**
  * Extension of default <code>AsyncAuditLogReceiver</code> that is MessageListener to allow entity
@@ -14,13 +14,13 @@ import it.reply.workflowManager.spring.orchestrator.annotations.WorkflowPersiste
  * for JMS environments - like MDB.
  */
 @Component
-public class AuditJMSProcessorBean extends AsyncAuditLogReceiver {
+public class AuditJmsProcessorBean extends AsyncAuditLogReceiver {
 
   @Autowired
   @WorkflowPersistenceUnit
   private EntityManagerFactory entityManagerFactory;
 
-  public AuditJMSProcessorBean() {
+  public AuditJmsProcessorBean() {
     super(null);
   }
 
--- workflow-manager-spring/src/main/java/it/reply/workflowManager/spring/orchestrator/jms/JmsSignalReceiverBean.java
+++ workflow-manager-spring/src/main/java/it/reply/workflowManager/spring/orchestrator/jms/JmsSignalReceiverBean.java
@@ -2,6 +2,6 @@ package it.reply.workflowManager.spring.orchestrator.jms;
 
 import org.jbpm.process.workitem.jms.JMSSignalReceiver;
 
-public class JMSSignalReceiverBean extends JMSSignalReceiver {
+public class JmsSignalReceiverBean extends JMSSignalReceiver {
 
 }
