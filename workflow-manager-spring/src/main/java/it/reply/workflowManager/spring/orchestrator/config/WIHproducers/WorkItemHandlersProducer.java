9d720c325cc4a19f6a934f8929cd1d931be983e0
Alberto Brigandì <a.brigandi@reply.it>
2016-02-05 17:57:16 +0100

    Testing fixes

--- workflow-manager-spring/src/main/java/it/reply/workflowManager/spring/orchestrator/config/WIHproducers/WorkItemHandlersProducer.java
+++ workflow-manager-spring/src/main/java/it/reply/workflowManager/spring/orchestrator/config/WIHproducers/WorkItemHandlersProducer.java
@@ -1,15 +1,16 @@
 package it.reply.workflowManager.spring.orchestrator.config.WIHproducers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
 import it.reply.workflowManager.orchestrator.bpm.commands.DispatcherCommand;
 import it.reply.workflowManager.orchestrator.config.WIHproducers.AbstractWorkItemHandlersProducer;
 import it.reply.workflowManager.spring.orchestrator.bpm.commands.SpringDispatcherCommand;
 
 import org.kie.api.executor.ExecutorService;
+import org.kie.api.runtime.process.WorkItemHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 /**
  * Producer of{@link WorkItemHandler}s to bind jBPM task to commands.
--- workflow-manager-spring/src/main/java/it/reply/workflowManager/spring/orchestrator/config/WorklfowPersistenceConfig.java
+++ workflow-manager-spring/src/main/java/it/reply/workflowManager/spring/orchestrator/config/WorklfowPersistenceConfig.java
@@ -1,44 +1,26 @@
 package it.reply.workflowManager.spring.orchestrator.config;
 
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-import javax.sql.DataSource;
+import it.reply.workflowManager.spring.orchestrator.annotations.WorkflowPersistenceUnit;
+
 import org.jbpm.executor.ExecutorServiceFactory;
 import org.kie.api.executor.ExecutorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import it.reply.workflowManager.spring.orchestrator.annotations.WorkflowPersistenceUnit;
-import it.reply.workflowManager.utils.Constants;
+import javax.naming.NamingException;
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
 @EnableTransactionManagement(proxyTargetClass = true)
 public class WorklfowPersistenceConfig {
 
   @Bean
-  public static DataSource workflowDataSource() throws NamingException {
-    Context ctx = new InitialContext();
-    return (DataSource) ctx.lookup("java:jboss/datasources/WorkflowManager/JBPM-DS");
-  }
-
-  @Bean
-  @WorkflowPersistenceUnit
-  public LocalContainerEntityManagerFactoryBean workflowEntityManagerFactory()
+  @Autowired
+  public ExecutorService executorService(@WorkflowPersistenceUnit EntityManagerFactory emf)
       throws NamingException {
-    LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-    factory.setPersistenceUnitName(Constants.PERSISTENCE_UNIT_NAME);
-    factory.setPersistenceXmlLocation("classpath:/META-INF/persistence.xml");
-    factory.setJtaDataSource(workflowDataSource());
-    return factory;
-  }
-
-  @Bean
-  public ExecutorService executorService() throws NamingException {
-    ExecutorService executorService = ExecutorServiceFactory
-        .newExecutorService(workflowEntityManagerFactory().getObject());
+    ExecutorService executorService = ExecutorServiceFactory.newExecutorService(emf);
     return executorService;
   }
 }
